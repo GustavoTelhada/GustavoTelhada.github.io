b280576c2b8307932e210f1ab85260b40b1a0c4c
GustavoTelhada <71329527+GusTelhada@users.noreply.github.com>
2021-03-15 11:23:36 +0000

    update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 function bgChanger(){
     if(window.scrollY > window.innerHeight / 2){
-        
+        document.body.classList.add("bg-active");
+    }
+    else{
+        document.body.classList.remove("bg-active"); 
     }
 }
 
